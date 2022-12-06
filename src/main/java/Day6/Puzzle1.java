5768a534f4f7f5a7459534ff288b6aac1ed5f021
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2022-12-06 21:23:14 +0100

    add day 6 puzzle solutions

---
+++ src/main/java/Day6/Puzzle1.java
@@ -0,0 +1,38 @@
+package Day6;
+
+import java.io.*;
+
+public class Puzzle1 {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/java/Day6/input.txt");
+        InputStream inputStream = new FileInputStream(file);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        if (reader.ready()){
+            String line = reader.readLine();
+            int markerAppearancePosition = getMarkerAppearancePosition(line);
+            System.out.println("Marker appearance postition: " + markerAppearancePosition);
+        }
+    }
+
+    private static int getMarkerAppearancePosition(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            boolean markerAppearance = checkIfMarkerAppears(line.substring(i, i+4));
+            if (markerAppearance){
+                return i+4;
+            }
+        }
+        return 0;
+    }
+
+    private static boolean checkIfMarkerAppears(String substring) {
+        for (int j = 0; j < substring.length(); j++) {
+            for (int i = 0; i < substring.length(); i++) {
+                if (substring.charAt(j) == substring.charAt(i) && j != i){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
